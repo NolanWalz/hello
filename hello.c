b8a41d5fa32d4a439c66e611994e4610a1b6f4be
Dalton Gregory <dgregory3@huskers.unl.edu>
2019-08-29 14:14:55 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Nolan and Dalton
+ * Date: 2019/27/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Nolan and Dalton are the best!\n");
+
+  return 0;
+}
